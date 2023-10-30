1e251af30940dfe52909213127d0730e78ba5c82
Othman kharbouch <otmankharbouch813@gmail.com>
2023-10-30 15:17:09 +0100

    Meging localy all items (1 bug found)

--- main-script.js
+++ main-script.js
@@ -1,6 +1,6 @@
 function addCol(e) {
     e.insertAdjacentHTML('beforebegin',`
-        <div class="list" draggable="true" style="animation-name:pop-down;animation-duration:0.5s;animation-timing-function:linear;">
+        <div class="list" draggable="true" style="animation-name:pop-down;animation-duration:0.2s;animation-timing-function:linear;">
             <div class="list-name" contenteditable="true" spellcheck="false">To Do</div>
             <div class="add"><img src="Assets/plus.svg" alt="" class="plus" onclick="addTask(this.parentNode)"></div>
         </div>
@@ -8,7 +8,7 @@ function addCol(e) {
 }
 function addTask(e) {
     e.insertAdjacentHTML('beforebegin',`
-    <div class="task" contenteditable="true" spellcheck="false" style="animation-name:pop-down;animation-duration:0.5s;animation-timing-function:linear;">New task <img src="Assets/dots-3.png" class="dots" alt="" onclick="showModal1(this)"></div>
+    <div class="task" contenteditable="true" spellcheck="false" style="animation-name:pop-down;animation-duration:0.2s;animation-timing-function:linear;">New task <img src="Assets/dots-3.png" class="dots" alt="" onclick="showModal1(this)"></div>
     `)
 }
 function showModal1(e) {
