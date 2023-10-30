fc6a9c204f08ee963dc6dda68a9880adf77cabbb
EL-Nabil-Hakimi <nabilelhakimi2023@gmail.com>
2023-10-30 16:24:34 +0100

    Page 1 Complet

---
+++ main-script.js
@@ -0,0 +1,23 @@
+function addCol(e) {
+    e.insertAdjacentHTML('beforebegin',`
+        <div class="list" draggable="true" style="animation-name:pop-down;animation-duration:0.2s;animation-timing-function:linear;">
+            <div class="list-name" contenteditable="true" spellcheck="false">To Do</div>
+            <div class="add"><img src="Assets/plus.svg" alt="" class="plus" onclick="addTask(this.parentNode)"></div>
+        </div>
+    `);
+}
+function addTask(e) {
+    e.insertAdjacentHTML('beforebegin',`
+    <div class="task" contenteditable="true" spellcheck="false" style="animation-name:pop-down;animation-duration:0.2s;animation-timing-function:linear;">New task <img src="Assets/dots-3.png" class="dots" alt="" onclick="showModal1(this)"></div>
+    `)
+}
+function showModal1(e) {
+    console.log(e.src);
+    if (e.src == 'http://127.0.0.1:3000/Assets/dots-3.png') {
+        e.src = 'Assets/dots-3-after.png';
+        document.getElementById('main').style.filter = 'blur(5px)';
+    } else if (e.src = 'http://127.0.0.1:3000/Assets/dots-3-after.png') {
+        e.src = 'Assets/dots-3.png';
+        document.getElementById('main').style.filter = 'blur(0px)';
+    }
+}
